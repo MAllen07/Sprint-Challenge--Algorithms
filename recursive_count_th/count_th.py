a71272e275973dc158ddcb8dfa89d8995ed5cf21
Mindy Allen <mrsmallen07@gmail.com>
2019-10-11 12:16:18 -0400

    debugging and testing error

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -1,5 +1,5 @@
 '''
-Your function should take in a signle parameter (a string `word`)
+Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
@@ -20,7 +20,7 @@ def count_th(word):
       word_leng = len(word)
      if word_leng == 0 or word_leng < 2:
        return 0
-   elif word[0:2] == “th”:
+   elif word[0:2] == "th":
        return count_th(word[1::]) + 1
    else:
        return count_th(word[1::])
